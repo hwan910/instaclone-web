5a75ccbaa6ca6168ab42683cbe172e45d2391720
sungwhanPark <william@chicment.com>
2023-06-09 01:13:51 +0900

    setting global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { useReactiveVar } from "@apollo/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { darkModeVar, isLoggedInVar } from "./apollo";
+import Home from "./screen/Home";
+import Login from "./screen/Login";
+import NotFound from "./screen/NotFound";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles, darkTheme, lightTheme } from "./style";
+
 function App() {
-  return <div>123</div>;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={isLoggedIn ? <Home /> : <Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/screen/Login.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.fontColor};
+`;
+
+const Container = styled.div``;
+
+const Login = () => {
+  return (
+    <Container>
+      <Title>Login</Title>
+    </Container>
+  );
+};
+
+export default Login;
